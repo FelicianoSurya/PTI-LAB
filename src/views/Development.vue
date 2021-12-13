48fcf85b32caff76c2323382a988404a2cc41c97
Deiyak <dea.noveriyanti@student.umn.ac.id>
2021-12-13 16:26:03 +0700

    gambar

--- src/views/Development.vue
+++ src/views/Development.vue
@@ -6,21 +6,21 @@
           <div class="row justify-content-center">
               <div class="col-lg-3 col-sm-6 col-8 p-3">
                   <div class="ourteam-card text-center">
-                      <img src="../assets/images/Ano.png" width="100%" alt="">
+                      <img src="../assets/images/Ano.png" alt="">
                       <h4><b>Feliciano Surya M</b></h4>
                       <p class="m-0">( 000 000 43255 )</p>
                   </div>
               </div>
               <div class="col-lg-3 col-sm-6 col-8 p-3">
                   <div class="ourteam-card text-center">
-                      <img src="../assets/images/Dea.png" width="100%" alt="">
+                      <img src="../assets/images/Dea.png" alt="">
                       <h4><b>Dea Noveriyanti</b></h4>
                       <p class="m-0">( 000 000 42550 )</p>
                   </div>
               </div>
               <div class="col-lg-3 col-sm-6 col-8 p-3">
                   <div class="ourteam-card text-center">
-                      <img src="../assets/images/Charisma.png" width="100%" alt="">
+                      <img src="../assets/images/Charisma.png" alt="">
                       <h4><b>Charisma Christ A</b></h4>
                       <p class="m-0">( 000 000 43903 )</p>
                   </div>
